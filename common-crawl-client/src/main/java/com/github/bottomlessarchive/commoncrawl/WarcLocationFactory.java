ec74540729b0b71e6eda0161afb4ffd5764f1eac
Gyula Lakatos <laxika91@gmail.com>
2020-01-16 16:28:50 +0100

    Added stream based factory methods to the WarcLocationFactory.

--- common-crawl-client/src/main/java/com/github/bottomlessarchive/commoncrawl/WarcLocationFactory.java
+++ common-crawl-client/src/main/java/com/github/bottomlessarchive/commoncrawl/WarcLocationFactory.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.davidmoten.io.extras.IOUtil;
 
@@ -23,13 +24,29 @@ public class WarcLocationFactory {
     private static final String AWS_S3_PREFIX = "https://commoncrawl.s3.amazonaws.com/";
 
     /**
-     * Return the locations of the WARC files as {@link URL}s that belong to the provided Common Crawl crawl id.
+     * Return the locations of the WARC files as a {@link List} of {@link URL}s that belong to the provided
+     * Common Crawl crawl id.
      *
      * @param crawlId the id of the Common Crawl crawl to get the locations for
      * @return the locations of the WARC files
      */
-    public List<URL> newUrls(final String crawlId) {
-        return newLocations(crawlId).stream()
+    public List<URL> buildLocationUrlList(final String crawlId) {
+        try (final Stream<URL> stream = buildLocationUrlStream(crawlId)) {
+            return stream.collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * Return the locations of the WARC files as a {@link Stream} of {@link URL}s that belong to the provided
+     * Common Crawl crawl id.
+     * <p>
+     * The returned stream should by closed by the caller of this method.
+     *
+     * @param crawlId the id of the Common Crawl crawl to get the locations for
+     * @return the locations of the WARC files
+     */
+    public Stream<URL> buildLocationUrlStream(final String crawlId) {
+        return buildLocationStringStream(crawlId)
             .map(location -> {
                 try {
                     return new URL(location);
@@ -37,27 +54,48 @@ public class WarcLocationFactory {
                     //Should never be thrown
                     throw new RuntimeException("Unable to convert WARC url: " + location + "!", e);
                 }
-            })
-            .collect(Collectors.toList());
+            });
     }
 
     /**
-     * Return the locations of the WARC files that belong to the provided Common Crawl crawl id.
+     * Return the locations of the WARC files that belong to the provided Common Crawl crawl id as a {@link List}.
      *
      * @param crawlId the id of the Common Crawl crawl to get the locations for
      * @return the locations of the WARC files
      */
-    public List<String> newLocations(final String crawlId) {
-        try (final BufferedReader downloadPathsReader = downloadLocations(crawlId)) {
+    public List<String> buildLocationStringList(final String crawlId) {
+        try (final Stream<String> stream = buildLocationStringStream(crawlId)) {
+            return stream.collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * Return the locations of the WARC files that belong to the provided Common Crawl crawl id as a {@link Stream}.
+     * <p>
+     * The returned stream should by closed by the caller of this method.
+     *
+     * @param crawlId the id of the Common Crawl crawl to get the locations for
+     * @return the locations of the WARC files
+     */
+    public Stream<String> buildLocationStringStream(final String crawlId) {
+        try {
+            final BufferedReader downloadPathsReader = buildWarcReader(crawlId);
+
             return downloadPathsReader.lines()
                 .map(this::buildLocation)
-                .collect(Collectors.toList());
-        } catch (IOException e) {
+                .onClose(() -> {
+                    try {
+                        downloadPathsReader.close();
+                    } catch (final IOException e) {
+                        throw new RuntimeException("Unable to load WARC file paths.", e);
+                    }
+                });
+        } catch (final IOException e) {
             throw new RuntimeException("Unable to load WARC file paths.", e);
         }
     }
 
-    private BufferedReader downloadLocations(final String pathsLocation) throws IOException {
+    private BufferedReader buildWarcReader(final String pathsLocation) throws IOException {
         final InputStream warcPathLocation = IOUtil.gunzip(new URL(AWS_S3_PREFIX + "crawl-data/"
             + pathsLocation + "/warc.paths.gz").openStream());
 
